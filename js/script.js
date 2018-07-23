c1f1d499f1b44db39cd1910a499d529b2fcf7c40
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-07-23 21:20:59 +0000

    challenge 4

--- js/script.js
+++ js/script.js
@@ -9,9 +9,26 @@ $("img").click(function(){
 	$(this).next().children('p').slideDown();	 
 });
 
-
+//when card is clicked it is assigned a class (.highlight) which toggles on/off as clicked
+//this one overrides one below
 $(".card").click(function(){
-    $(this).toggleClass("highlight");
-})
+    $(this).toggleClass("highlight");       //highlight is pink
+});
+
+
+//when card is clicked it is assigned a class (.highlight_stream) which toggles on/off as clicked
+$(".card").click(function(){
+    $(this).toggleClass("highlight_stream"); //highloight_stream is red
+});
+
+//cards not currently selected will be hidden when select_btn is clicked
+$("#select_btn").click(function(){
+    $(".card:not(.highlight)").hide();
+});
+
+//all cards will now be shown
+$("#all_btn").click(function(){
+    $(".card").show();
+});
 }); 
 
